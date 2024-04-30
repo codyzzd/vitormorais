07eb017ebdc7791d0a7c3ba206579d0c50b9b526
codyzzd <codyzzd@gmail.com>
2024-04-30 18:43:58 -0300

    fix layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
-import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 import { Bebas_Neue, Inter } from "next/font/google";
 import { Suspense } from "react";
