2d6db783de6bbe39e459094b0d874a43987fc2a3
codyzzd <codyzzd@gmail.com>
2024-04-30 21:51:09 -0300

    push some content

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
@@ -6,46 +7,133 @@ export default function Home() {
       <div className="bg-black text-white">
         {/* Hero */}
         <section
-        /*style={{
+          style={{
             backgroundImage: 'url("/images/back_hero.avif")',
             backgroundSize: "cover",
             backgroundPosition: "center",
-          }}*/
-        >
-          <div className="container flex flex-col mx-auto px-6 py-8 md:px-8 md:py-16   xl:px-48 xl:py-20">
-            <p>logo</p>
-            <h1 className="bebas">
-              transforme seu corpo treinando no conforto da sua casa com poucos
-              minutos do seu dia
-            </h1>
-            <p>
-              Obtenha acesso ao meu método de treino e veja como é simples
-              <strong>perder peso, ganhar força e definir o corpo</strong> em
-              treinos que tomam menos de 30 minutos do seu dia.
-            </p>
-            <p>Quero ter acesso ao Método Vitor Morais</p>
-            <p>De 12x de R$ 39,90 por 12x de R$ 19,90 no plano anual</p>
+          }}>
+          {/* not mobile */}
+          <div className="container flex flex-col  box-1 hidden">
+            <div id="hero_image"></div>
+            <div
+              id="hero_image_mobile"
+              className="w-full h-[600px]"
+              style={{
+                backgroundImage: 'url("/images/vitorraio.avif")',
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+              }}></div>
+            <div id="hero_content" className="flex flex-col gap-4">
+              <Image
+                className="w-1/2"
+                src="/images/vitorlogo.avif"
+                width={164}
+                height={164}
+                alt="Logo Método Vitor Morais"
+              />
+              <h1 className="bebas">
+                Transforme seu corpo treinando no conforto da sua casa com
+                poucos minutos do seu dia
+              </h1>
+              <p className="">
+                Obtenha acesso ao meu método de treino e veja como é simples{" "}
+                <strong>perder peso, ganhar força e definir o corpo</strong> em
+                treinos que tomam menos de 30 minutos do seu dia.
+              </p>
+              <Link href="/">
+                <p className="green-button">
+                  Quero ter acesso ao Método Vitor Morais
+                </p>
+              </Link>
+              <p className="">
+                De 12x de R$ 39,90 por 12x de R$ 19,90 no plano anual
+              </p>
+              d
+            </div>
+          </div>
+          {/* mobile */}
+          <div
+            id="hero_image_mobile"
+            className="w-full h-[500px]"
+            style={{
+              backgroundImage: 'url("/images/vitorraio.avif")',
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+            }}></div>
+          <div className=" flex flex-col container  px-6 py-8">
+            <div id="hero_content" className="flex flex-col gap-4">
+              <Image
+                className="w-1/2"
+                src="/images/vitorlogo.avif"
+                width={164}
+                height={164}
+                alt="Logo Método Vitor Morais"
+              />
+              <h1 className="bebas">
+                Transforme seu corpo treinando no conforto da sua casa com
+                poucos minutos do seu dia
+              </h1>
+              <p className="text-xl">
+                Obtenha acesso ao meu método de treino e veja como é simples{" "}
+                <strong>perder peso, ganhar força e definir o corpo</strong> em
+                treinos que tomam menos de 30 minutos do seu dia.
+              </p>
+              <Link href="/">
+                <p className="green-button">CTA</p>
+              </Link>
+              <p className="">
+                De 12x de R$ 39,90 por 12x de R$ 19,90 no plano anual
+              </p>
+            </div>
           </div>
         </section>
 
         {/* 4 Fotos */}
         <section>
-          <div className="container flex flex-col mx-auto px-6 py-8 md:px-8 md:py-16   xl:px-48 xl:py-20 text-center">
+          <div className="container flex flex-col  box-1 space-y-4">
             <h2 className="bebas">
               Quem treinou comigo comprova os resultados
             </h2>
+            <div className="flex flex-col space-y-4">
+              <Image
+                className="w-full"
+                src="/images/prova1.avif"
+                width={541}
+                height={573}
+                alt="Prova 1"
+              />
+              <Image
+                className="w-full"
+                src="/images/prova2.avif"
+                width={541}
+                height={573}
+                alt="Prova 2"
+              />
+              <Image
+                className="w-full"
+                src="/images/prova3.avif"
+                width={541}
+                height={574}
+                alt="Prova 3"
+              />
+              <Image
+                className="w-full"
+                src="/images/prova4.avif"
+                width={541}
+                height={574}
+                alt="Prova 4"
+              />
+            </div>
             <Link href="/">
-              <p className="green-button">
-                Quero ter acesso ao Método Vitor Morais
-              </p>
+              <p className="green-button">CTA</p>
             </Link>
           </div>
         </section>
 
         {/* O que é o metodo vitor morais */}
         <section>
-          <div className="container flex flex-col mx-auto px-6 py-8 md:px-8 md:py-16   xl:px-48 xl:py-20 text-center">
-            <h2 className="bebas">O que é o Método Vitor Morais?</h2>
+          <div className="container flex flex-col  box-1 space-y-4">
+            <h2 className="bebas w-3/4">O que é o Método Vitor Morais?</h2>
             <p>
               <strong>Sua iniciação para sua nova vida fitness!</strong>
             </p>
@@ -62,23 +150,22 @@ export default function Home() {
               </strong>
             </p>
             <Link href="/">
-              <p className="green-button">
-                Quero ter acesso ao Método Vitor Morais
-              </p>
+              <p className="green-button">CTA</p>
             </Link>
           </div>
         </section>
 
         {/* alem disso voce tera */}
-        <section className="bg-[#F28504] text-white">
-          <div className="container flex flex-col mx-auto px-6 py-8 md:px-8 md:py-16   xl:px-48 xl:py-20 text-center">
-            <h2 className="bebas">Além disso, você terá acesso a:</h2>
+        <section className="bg-orange-600 text-white">
+          <div className="container flex flex-col box-1">
+            <h2 className="bebas w-3/4">Além disso, você terá acesso a:</h2>
+            
           </div>
         </section>
 
         {/* veja de onde quiser */}
         <section>
-          <div className="container flex flex-col mx-auto px-6 py-8 md:px-8 md:py-16   xl:px-48 xl:py-20 text-center">
+          <div className="container flex flex-col box-1 text-center">
             <h2 className="bebas">Veja de onde você quiser!</h2>
 
             <Link href="/">
@@ -91,7 +178,7 @@ export default function Home() {
 
         {/* tabela */}
         <section>
-          <div className="container flex flex-col mx-auto px-6 py-8 md:px-8 md:py-16   xl:px-48 xl:py-20 text-center">
+          <div className="container flex flex-col  box-1 text-center">
             <h2 className="bebas">
               Chegou a sua vez de entrar em forma e alcançar o corpo perfeito!
             </h2>
@@ -99,15 +186,15 @@ export default function Home() {
         </section>
 
         {/* chamada promo */}
-        <section className="bg-[#F28504] text-white">
-          <div className="container flex flex-col mx-auto px-6 py-8 md:px-8 md:py-16   xl:px-48 xl:py-20 ">
+        <section className="bg-orange-600 text-white">
+          <div className="container flex flex-col  box-1 ">
             <h2 className="bebas">Veja tudo que você vai receber agora!</h2>
           </div>
         </section>
 
         {/* prazer sou vitor */}
         <section>
-          <div className="container flex flex-col mx-auto px-6 py-8 md:px-8 md:py-16   xl:px-48 xl:py-20 ">
+          <div className="container flex flex-col  box-1 ">
             <h2 className="bebas">Prazer, sou o vitor morais </h2>
             <p>
               Campeão Sul-americano de Calistenia e Street Workout e quinto
@@ -133,7 +220,7 @@ export default function Home() {
 
         {/* 7 dias sem risco strip */}
         <section>
-          <div className="container flex flex-col mx-auto px-6 py-8 md:px-8 md:py-16   xl:px-48 xl:py-20 ">
+          <div className="container flex flex-col  box-1 ">
             <h2 className="bebas">7 Dias sem risco!</h2>
             <p>
               Experimente meu método sem risco nenhum, se por algum motivo você
@@ -145,7 +232,7 @@ export default function Home() {
 
         {/* last call */}
         <section>
-          <div className="container flex flex-col mx-auto px-6 py-8 md:px-8 md:py-16   xl:px-48 xl:py-20 text-center">
+          <div className="container flex flex-col  box-1 text-center">
             <h2 className="bebas">Essa promoção não vai se repetir!</h2>
             <Link href="/">
               <p className="green-button uppercase">Eu quero!</p>
@@ -155,7 +242,7 @@ export default function Home() {
 
         {/* faq */}
         <section>
-          <div className="container flex flex-col mx-auto px-6 py-8 md:px-8 md:py-16   xl:px-48 xl:py-20 ">
+          <div className="container flex flex-col  box-1 ">
             <h2 className="bebas text-center">Perguntas frequentes</h2>
           </div>
         </section>
