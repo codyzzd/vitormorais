4849c7b65cb21813286faefb40208d92d3a55ca4
codyzzd <codyzzd@gmail.com>
2024-05-02 19:14:10 -0300

    mobile ready

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import {
   faCircleCheck,
   faLaptop,
   faMobile,
+  faStar,
   faTv,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -90,7 +91,7 @@ export default function Home() {
                   em treinos que tomam menos de 30 minutos do seu dia.
                 </p>
                 <Link href="/" className="">
-                  <p className="green-button w-full text-center">CTA</p>
+                  <p className="green-button w-full text-center">Quero o Método Vitor Morais</p>
                 </Link>
                 <p className="">
                   De 12x de R$ 39,90 por 12x de R$ 19,90 no plano anual
@@ -108,28 +109,28 @@ export default function Home() {
             </h2>
             <div className="flex flex-col space-y-4">
               <Image
-                className="w-full"
+                className="w-full rounded-2xl"
                 src="/images/prova1.avif"
                 width={541}
                 height={573}
                 alt="Prova 1"
               />
               <Image
-                className="w-full"
+                className="w-full rounded-2xl"
                 src="/images/prova2.avif"
                 width={541}
                 height={573}
                 alt="Prova 2"
               />
               <Image
-                className="w-full"
+                className="w-full rounded-2xl"
                 src="/images/prova3.avif"
                 width={541}
                 height={574}
                 alt="Prova 3"
               />
               <Image
-                className="w-full"
+                className="w-full rounded-2xl"
                 src="/images/prova4.avif"
                 width={541}
                 height={574}
@@ -137,7 +138,7 @@ export default function Home() {
               />
             </div>
             <Link href="/" className="">
-              <p className="green-button w-full text-center">CTA</p>
+              <p className="green-button w-full text-center">Quero o Método Vitor Morais</p>
             </Link>
           </div>
         </section>
@@ -156,14 +157,14 @@ export default function Home() {
             </p>
 
             <Image
-              className="w-full"
+              className="w-full rounded-2xl"
               src="/images/exec_avan.avif"
               width={1091}
               height={1229}
               alt="Exercícios Avançados"
             />
             <Image
-              className="w-full"
+              className="w-full rounded-2xl"
               src="/images/exec_adap.avif"
               width={1092}
               height={1229}
@@ -178,7 +179,7 @@ export default function Home() {
               </strong>
             </p>
             <Link href="/" className="">
-              <p className="green-button w-full text-center">CTA</p>
+              <p className="green-button w-full text-center">Quero o Método Vitor Morais</p>
             </Link>
           </div>
         </section>
@@ -227,14 +228,14 @@ export default function Home() {
             <h2 className="bebas">Veja de onde você quiser!</h2>
 
             <div className="flex flex-col space-y-4 text-xl font-bold">
-              <div className="p-8 flex   gap-4 border-slate-700 border-[1px] items-center">
+              <div className="p-8 flex   gap-4 border-slate-700 border-[1px] items-center rounded-2xl">
                 <FontAwesomeIcon icon={faTv} size="2x" className="w-[48px]" />
                 <div>
                   Faça as aulas na sua{" "}
                   <span className="text-orange-600 inline">Smart TV</span>
                 </div>
               </div>
-              <div className="p-8 flex gap-4 border-slate-700 border-[1px] items-center">
+              <div className="p-8 flex gap-4 border-slate-700 border-[1px] items-center rounded-2xl">
                 <FontAwesomeIcon
                   icon={faLaptop}
                   size="2x"
@@ -245,7 +246,7 @@ export default function Home() {
                   <span className="text-orange-600 inline">computador</span>
                 </div>
               </div>
-              <div className="p-8 flex gap-4 border-slate-700 border-[1px] items-center">
+              <div className="p-8 flex gap-4 border-slate-700 border-[1px] items-center rounded-2xl">
                 <FontAwesomeIcon
                   icon={faMobile}
                   size="2x"
@@ -261,7 +262,7 @@ export default function Home() {
             </div>
 
             <Link href="/" className="">
-              <p className="green-button w-full text-center">CTA</p>
+              <p className="green-button w-full text-center">Quero o Método Vitor Morais</p>
             </Link>
           </div>
         </section>
@@ -288,28 +289,30 @@ export default function Home() {
             <h2 className="bebas">Veja tudo que você vai receber agora!</h2>
             <div id="list-veja" className="font-bold text-lg space-y-2">
               <div className="flex gap-2">
-                <FontAwesomeIcon icon={faCircleCheck} className="mt-1" />
+                <FontAwesomeIcon icon={faCircleCheck} className="mt-1 text-[#8FFF00]" />
                 Método Vitor Morais
               </div>
               <div className="flex gap-2">
-                <FontAwesomeIcon icon={faCircleCheck} className="mt-1" />
+                <FontAwesomeIcon icon={faCircleCheck} className="mt-1 text-[#8FFF00]" />
                 Comunidade Exclusiva
               </div>
               <div className="flex gap-2">
-                <FontAwesomeIcon icon={faCircleCheck} className="mt-1" />
+                <FontAwesomeIcon icon={faCircleCheck} className="mt-1 text-[#8FFF00]" />
                 Lives Exclusivas
               </div>
               <div className="flex gap-2">
-                <FontAwesomeIcon icon={faCircleCheck} className="mt-1" />
+                <FontAwesomeIcon icon={faCircleCheck} className="mt-1 text-[#8FFF00]" />
                 Bônus: Acesso gratuito aos próximos protocolos de treino
               </div>
             </div>
-            <div className="bg-slate-900 p-8 space-y-8 text-center">
-              <h3 className="bebas">Promoção de Lançamento</h3>
+            <div className="bg-slate-900 p-8 space-y-8 text-center rounded-2xl">
+              <h2 className="bebas">Promoção de Lançamento</h2>
               <div className="space-y-3">
-                <p className="font-bold">De R$ 197,00 por 12x de</p>
-                <p className="font-bold text-[#8FFF00] text-6xl">R$15,90</p>
-                <p className="font-bold">ou R$ 147,00 à vista</p>
+                <p className="font-bold">De <span className="line-through">R$197,00</span> por 12x de</p>
+                <p className="font-bold text-[#8FFF00] text-8xl bebas">
+                  R$19,90
+                </p>
+                <p className="font-bold">ou R$147,00 à vista</p>
               </div>
               <Link href="/" className="inline-block w-full">
                 <p className="green-button w-full text-center uppercase p-6">
@@ -322,18 +325,18 @@ export default function Home() {
 
         {/* prazer sou vitor */}
         <section>
-          <div className="container flex flex-col  box-1 space-y-4">
+          <div className="container flex flex-col  box-1 space-y-4 ">
             <div
               style={{
                 backgroundImage: 'url("/images/vitor.avif")',
                 backgroundSize: "cover",
                 backgroundPosition: "center",
               }}
-              className="w-full h-[350px]"></div>
+              className="w-full h-[350px] rounded-2xl "></div>
 
-            <h2 className="bebas text-6xl">
+            <h1 className="bebas w-3/4 ">
               Prazer, sou o <span className="text-[#8FFF00]">vitor morais</span>
-            </h2>
+            </h1>
             <p>
               Campeão Sul-americano de Calistenia e Street Workout e quinto
               colocado mundial no esporte.
@@ -357,9 +360,22 @@ export default function Home() {
         </section>
 
         {/* 7 dias sem risco strip */}
-        <section>
-          <div className="container flex flex-col  box-1 ">
+        <section
+          style={{
+            backgroundImage:
+              'linear-gradient(to right, rgba(0,0,0,0.8), rgba(0,0,0,0.4)), url("/images/pendurado_gray.avif")',
+            backgroundSize: "cover",
+            backgroundRepeat: "no-repeat",
+          }}>
+          <div className="container flex flex-col  box-1 space-y-4">
             <h2 className="bebas">7 Dias sem risco!</h2>
+            <div className="flex text-yellow-400">
+              <FontAwesomeIcon icon={faStar} size="2x" />
+              <FontAwesomeIcon icon={faStar} size="2x" />
+              <FontAwesomeIcon icon={faStar} size="2x" />
+              <FontAwesomeIcon icon={faStar} size="2x" />
+              <FontAwesomeIcon icon={faStar} size="2x" />
+            </div>
             <p>
               Experimente meu método sem risco nenhum, se por algum motivo você
               não gostar do conteúdo devolvemos seu investimento sem perguntas
@@ -370,18 +386,52 @@ export default function Home() {
 
         {/* last call */}
         <section>
-          <div className="container flex flex-col  box-1 text-center">
-            <h2 className="bebas">Essa promoção não vai se repetir!</h2>
-            <Link href="/">
-              <p className="green-button uppercase">Eu quero!</p>
-            </Link>
+          <div className="container flex flex-col  box-1 text-center space-y-4">
+            <div className="p-8 space-y-8 text-center rounded-2xl  border-[#8fff00] border-[1px]">
+              <h2 className="bebas">eSSA PROMOÇÃO NÃO VAI SE REPETIR!</h2>
+              <div className="space-y-3">
+                <p className="font-bold">De <span className="line-through">R$197,00</span> por 12x de</p>
+                <p className="font-bold text-[#8FFF00] text-8xl bebas">
+                  R$19,90
+                </p>
+                <p className="font-bold">ou R$147,00 à vista</p>
+              </div>
+              <Link href="/" className="inline-block w-full">
+                <p className="green-button w-full text-center uppercase p-6">
+                  Eu Quero!!!
+                </p>
+              </Link>
+            </div>
           </div>
         </section>
 
         {/* faq */}
         <section>
-          <div className="container flex flex-col  box-1 ">
-            <h2 className="bebas text-center">Perguntas frequentes</h2>
+          <div className="container flex flex-col  box-1 space-y-4 [&_h5]:text-[#8fff00] ">
+            <h2 className="bebas">Perguntas frequentes</h2>
+            <div className="pb-4 border-b-[1px] border-b-slate-700">
+              <h5>Posso parcelar?</h5>
+              <p>Sim. Poderá parcelar em até 5x.</p>
+            </div>
+            <div className="pb-4 border-b-[1px] border-b-slate-700">
+              <h5>Existe garantia?</h5>
+              <p>Sim. O curso tem garantia de 7 dias.</p>
+            </div>
+            <div className="pb-4 border-b-[1px] border-b-slate-700">
+              <h5>Quanto tempo tenho de acesso a plataforma?</h5>
+              <p>O curso terá acesso de um ano.</p>
+            </div>
+            <div className="pb-4 border-b-[1px] border-b-slate-700">
+              <h5>Como acesso a plataforma?</h5>
+              <p>O acesso chegará em seu e-mail.</p>
+            </div>
+            <div className="pb-4 border-b-[1px] border-b-slate-700">
+              <h5>Como faço para entrar na comunidade?</h5>
+              <p>
+                Dentro do Método Vitor Morais você encontrará os links para
+                entrar na comunidade.
+              </p>
+            </div>
           </div>
         </section>
       </div>
