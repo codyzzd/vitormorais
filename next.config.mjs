79276abdf08221d10495f12a3c9eb73c0b25e483
codyzzd <codyzzd@gmail.com>
2024-05-13 23:41:33 -0300

    rewrite3

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,5 @@
-/**  @type {import('next').NextConfig} */
-/*const nextConfig = {};
-
-export default nextConfig;*/
-/*
-/* @type {import('next').NextConfig} */
-/*const nextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   async rewrites() {
     return [
       {
@@ -15,16 +10,4 @@ export default nextConfig;*/
   },
 };
 
-export default nextConfig;*/
-
-
-module.exports = {
-  async rewrites() {
-    return [
-      {
-        source: '/:path*',
-        destination: 'https://helpful-music-830291.framer.app/:path*',
-      },
-    ]
-  },
-}
+export default nextConfig;
